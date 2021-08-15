3e7c9a233bbc5ac4f8c68e07836db953a9ac8730
tarun_72 <tarunshrm395@gmail.com>
2021-08-15 18:11:24 +0530

    not completed yet

--- src/array/DutchNationFlag.java
+++ src/array/DutchNationFlag.java
@@ -4,6 +4,8 @@ public class DutchNationFlag extends ElementsOfProgramming {
     @Override
     void onSolution() {
         sortColorsBruteFoce(new int[]{2, 0, 2, 1, 1, 0});
+        //sortColors(new int[]{2, 0, 2, 1, 1, 0});
+        sortColors(new int[]{1,2,0});
     }
 
     @Override
@@ -23,5 +25,51 @@ public class DutchNationFlag extends ElementsOfProgramming {
                 }
             }
         }
+    }
+    public void sortColors(int[] nums) {
+    int left = 0,right = nums.length-1;
+    quickSort(nums,left,right);
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(" "+nums[i]);
+        }
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        if(left < right){
+            int partition =  partition(nums,left,right);
+            quickSort(nums,left,partition-1);
+            quickSort(nums,partition+1,right);
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int start = left;
+        int end =  right;
+        int pivot =  (left+right)/2;
+        int pivotElements =  nums[pivot];
+        while(start<end){
+            while(start<end && nums[start]<pivotElements){
+                start++;
+            }
+            while(nums[end]>pivotElements){
+                end--;
+            }
+            if(start< end){
+                // swapping should be done
+                int temp =  nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        nums[start] = nums[right];
+        nums[right] = pivotElements;
+        return end;
+    }
+
+    public static void main(String[] args) {
+        new DutchNationFlag().onSolution();
     }
 }
